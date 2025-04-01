37418a238e1c5c32adf8b2854169c98e94e343b4
RaduTeodorProsie2 <abamon.game@gmail.com>
2025-04-01 14:42:55 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
-#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
-
     sf::Music instrumental;
     const bool open = instrumental.openFromFile("../assets/songs/careless/Inst.ogg");
     
